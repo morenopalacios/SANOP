f5bf6939f5326f4a21fe5984dea69504d137f711
ingry moreno <ingri_17@hotmail.com>
2014-08-18 21:35:33 -0500

    se termino el modulo de funcionario

--- app/models/riesgo.rb
+++ app/models/riesgo.rb
@@ -1,4 +1,10 @@
 class Riesgo < ActiveRecord::Base
+
+def self.search(search, page)
+where(['upper(empresa) like ?',
+"%#{search}%".upcase]).paginate(page: page, per_page: 5).order("empresa")
+end
+    
   belongs_to :funcionario
 
     
